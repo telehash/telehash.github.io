0fce22ffb3fa36de6632cc39fb96efec60c526f0
Thomas Muldowney <temas@sparecog.com>
2015-03-21 23:34:34 -0600

    More reference fixes. More content.

--- tools/md2htmlbook.js
+++ tools/md2htmlbook.js
@@ -10,6 +10,7 @@ var options = {
     xhtml:true
 };
 
+var dirPrefix = "v3";
 
 var bookOrg = JSON.parse(fs.readFileSync("book.json"));
 //console.log(bookOrg);
@@ -22,10 +23,49 @@ var outHTML = "";
 var renderer = new marked.Renderer();
 renderer.sectionLevel = 0;
 
+var curAnchorPrefix = "";
+function makeAnchor(baseName, section) {
+    
+    var anchorName = baseName;
+    anchorName = anchorName.replace("README", "");
+    anchorName = anchorName.replace(/\//g, "_")
+    anchorName = anchorName.replace(".md", "");
+    if (/\.\._/.test(anchorName)) {
+        var upPrefix = (section.parent && section.parent.prefix) || "";
+        anchorName = anchorName.replace("\.\._", upPrefix + ((upPrefix.length > 0) ? "_" : ""));
+    } else  if (curAnchorPrefix) {
+        var curPrefix = anchorName.substring(0, curAnchorPrefix.length + 1);
+        if (curPrefix != (curAnchorPrefix + "_")) {
+            
+            //console.log("Using prefix " + curAnchorPrefix + " on " + anchorName);
+            anchorName = curAnchorPrefix + "_" + anchorName;
+            //console.log("is now ", anchorName);
+        }
+    }
+    return anchorName;
+}
+
+function fileToAnchor(section) {
+    var parts = section.content.split(path.sep);
+    parts.shift();
+    if (parts[0] == dirPrefix) {
+        parts.shift();
+    }
+    return makeAnchor(parts.join(path.sep), section);
+}
+
 function processSection(count, depth, type, section) {
+    var prevSection = renderer.curSection;
+    renderer.curSection = section;
+    var prevPrefix = curAnchorPrefix;
+    if (section.prefix) {
+        //console.log("Set prefix to " + section.parent.prefix);
+        curAnchorPrefix = section.prefix;
+    }
     if (section.content) {
-        renderer.nameTag = path.basename(section.content);
-        outHTML += "<a id='" + path.basename(section.content) + "' name='" + path.basename(section.content) + "'></a>";
+        renderer.nameTag = fileToAnchor(section);
+        
+        outHTML += "<a id='" + fileToAnchor(section) + "' name='" + fileToAnchor(section) + "'></a>";
     }
     outHTML += "<a id='" + type + "-" + count + "-" + depth + "' name='" + type + "-" + count + "-" + depth + "'></a><section data-type='" + type + "'>";
     if (section.content) {
@@ -34,11 +74,16 @@ function processSection(count, depth, type, section) {
     }
     if (section.sections) {
         section.sections.forEach(function(sectionInfo) {
+            sectionInfo.parent = section;
             renderer.sectionLevel = depth + 1;
             processSection(count, depth + 1, "section", sectionInfo);
             renderer.sectionLevel = depth;
         });
     }
+
+    curAnchorPrefix = prevPrefix;
+    renderer.curSection = prevSection;
+
     outHTML += "</section>";
 }
 
@@ -62,7 +107,7 @@ renderer.link = function(href, title, text)
         return prevLinkRenderer.call(this, href, title, text);
     }
 
-    var out = '<a class="xref" href="#' + href + '"';
+    var out = '<a class="xref" href="#' + makeAnchor(href, this.curSection) + '"';
     if (title) {
         out += ' title="' + title + '"';
     }
